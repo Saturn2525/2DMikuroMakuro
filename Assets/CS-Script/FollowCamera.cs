1fc928cc9f3ff3e477a3e08347aa77414a83f6d3
harumas <haruto09.game@gmail.com>
2025-05-10 11:40:48 +0900

    カメラのスクリプト修正

--- Assets/CS-Script/FollowCamera.cs
+++ Assets/CS-Script/FollowCamera.cs
@@ -7,15 +7,8 @@ public class FollowCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform target; // 追従対象（プレイヤーなど）
-    [SerializeField] private float followSpeed = 5f; // 移動速度
-    [SerializeField] private float yOffset = 2f;
+    private float followSpeed = 5f; // 移動速度
     private float YAbs;
-    private float targetY;
-
-    private void Start()
-    {
-        targetY = target.position.y;
-    }
 
     void FixedUpdate()
     {
@@ -23,13 +16,13 @@ public class FollowCamera : MonoBehaviour
         Vector3 newPosition = transform.position;
         // X座標のみを追従
         newPosition.x = Mathf.Lerp(transform.position.x, target.position.x, followSpeed * Time.deltaTime);
-        // YAbs = Mathf.Abs(newPosition.y - target.position.y);
-        // if (target.position.y > 0 || YAbs > 3.5f)
-        // {
-        //     newPosition.y = Mathf.Lerp(transform.position.y, target.position.y, followSpeed * Time.deltaTime);
-        // }
+        YAbs = Mathf.Abs(newPosition.y - target.position.y);
+        if (target.position.y > 0 || YAbs > 3.5f)
+        {
+            newPosition.y = Mathf.Lerp(transform.position.y, target.position.y, followSpeed * Time.deltaTime);
+        }
 
         // 位置を更新
-        transform.position = new Vector3(newPosition.x, targetY + yOffset, newPosition.z);
+        transform.position = newPosition;
     }
 }
